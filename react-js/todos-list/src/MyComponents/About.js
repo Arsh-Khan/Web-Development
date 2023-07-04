a44e74c92e03152eb7175037f8e1547b94011125
Arsh Khan <khanarsh0124@gmail.com>
2023-07-05 01:03:10 +0530

    Added React-JS Course

---
+++ react-js/todos-list/src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is a about component
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum delectus eligendi nesciunt magnam, officia, eum enim exercitationem velit sapiente nam, temporibus iure soluta. Aliquid commodi error id officiis quidem odio earum minus illum expedita.</p>
+    </div>
+  )
+}
